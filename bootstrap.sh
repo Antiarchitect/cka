e76f7b16d07a1e13b28f3ace4202123cfff42c3b
Andrey Voronkov <voronkovaa@gmail.com>
2019-02-05 06:31:14 +0300

    Clone the proper branch.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ user="${1}"
 echo "${user}"
 
 su -c 'git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d' "${user}"
-su -c 'git clone https://github.com/Antiarchitect/.dotfiles ~/.dotfiles' "${user}"
+su -c 'git clone --branch cka https://github.com/Antiarchitect/.dotfiles ~/.dotfiles' "${user}"
 su -c 'ln -s ~/.dotfiles/.spacemacs ~/.spacemacs' "${user}"
 add-apt-repository ppa:kelleyk/emacs -y
 apt update -y
