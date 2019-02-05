0e0c016eef4996bd596a257905a0d97de001800b
Andrey Voronkov <voronkovaa@gmail.com>
2019-02-05 06:02:28 +0300

    And she sayed YES

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ echo "${user}"
 su -c 'git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d' "${user}"
 su -c 'git clone https://github.com/Antiarchitect/.dotfiles ~/.dotfiles' "${user}"
 su -c 'ln -s ~/.dotfiles/.spacemacs ~/.spacemacs' "${user}"
-add-apt-repository ppa:kelleyk/emacs
-apt update
-apt install emacs25
+add-apt-repository ppa:kelleyk/emacs -y
+apt update -y
+apt install emacs25 -y
