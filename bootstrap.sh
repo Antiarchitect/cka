dc62b8d804a15aac34c1fe4e3b7b6bb1e5bf47e0
Andrey Voronkov <voronkovaa@gmail.com>
2019-02-05 06:54:25 +0300

    Exit on error. Add install of fontconfig.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 if [ -z "${1}" ]; then
     echo "Please provide current username..."
     exit 1
@@ -14,6 +16,7 @@ su -c 'ln -s ~/.dotfiles/.spacemacs ~/.spacemacs' "${user}"
 fonts_dir="/usr/share/fonts/truetype"
 mkdir -p "${fonts_dir}"
 curl https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz | tar -xzv --directory "${fonts_dir}/SourceCodePro"
+apt install fontconfig -y
 fc-cache -f -v
 
 add-apt-repository ppa:kelleyk/emacs -y
