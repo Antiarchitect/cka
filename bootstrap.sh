dfa13e3b5d5222eef971234a752dcc7b145c6dc1
Andrey Voronkov <voronkovaa@gmail.com>
2019-02-05 05:57:42 +0300

    Fix wrong command

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@ user=$USER
 
 echo "${user}"
 
-su -c 'git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d "${user}"'
-su -c 'git clone https://github.com/Antiarchitect/.dotfiles ~/.dotfiles "${user}"'
-su -c 'ln -s ~/.dotfiles/.spacemacs ~/.spacemacs "${user}"'
+su -c 'git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d' "${user}"
+su -c 'git clone https://github.com/Antiarchitect/.dotfiles ~/.dotfiles' "${user}"
+su -c 'ln -s ~/.dotfiles/.spacemacs ~/.spacemacs' "${user}"
 add-apt-repository ppa:kelleyk/emacs
 apt update
 apt install emacs25
